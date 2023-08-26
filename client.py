af8f35a0deb311adc964db0da22e10406d3bed69
muxamilian <maximilian.bachl@gmail.com>
2023-08-26 19:18:35 +0200

    Changes

--- client.py
+++ client.py
@@ -2,18 +2,13 @@ import socket
 from select import select
 from common import *
 
-socks = []
-for i, port in enumerate(udp_ports):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socks.append(sock)
-    msg = packed_zero + \
-        packed_zero
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+msg = packed_zero + \
+    packed_zero
 
-    sock.sendto(msg, ("127.0.0.1", udp_ports[i]))
+sock.sendto(msg, ("127.0.0.1", udp_ports[0]))
 
 while True:
-    ready_socks,_,_ = select(socks, [], []) 
-    for sock in ready_socks:
-        data, addr = sock.recvfrom(minimum_payload_size)
-        msg = packed_zero + data[:4]
-        sock.sendto(msg, addr)
+    data, addr = sock.recvfrom(minimum_payload_size)
+    msg = packed_zero + data[:4]
+    sock.sendto(msg, addr)
--- server.py
+++ server.py
@@ -15,6 +15,7 @@ for port in udp_ports:
     sock.bind(('0.0.0.0', port))
 
 def send_and_receive_acks():
+    socks[0].setblocking(False)
     # last_rtt = None
     last_rtt = 1
     start_time = time.time()
@@ -22,33 +23,28 @@ def send_and_receive_acks():
     while True:
         current_time = time.time()
         # if last_rtt is not None and current_time > start_time + last_rtt:
-        if last_rtt is not None and current_time > start_time + 1:
+        if last_rtt is not None and current_time > start_time + 10:
             break
-        r,_,_ = select(socks,[],[],0)
-        for sock in r:
-            data, _ = sock.recvfrom(8)
+        # r,_,_ = select([socks[0]],[],[],0)
+        # for sock in r:
+        try:
+            data, _ = socks[0].recvfrom(8)
             ack_num = struct.unpack('!I', data[4:8])[0]
-            sock_index = socks.index(sock)
-            last_rtt = current_time - send_times[sock_index][ack_num]
-            del send_times[sock_index][ack_num]
+            # sock_index = socks.index(sock)
+            # last_rtt = current_time - send_times[sock_index][ack_num]
+            # del send_times[sock_index][ack_num]
+        except:
+            pass
 
         ret_msg = struct.pack('!I', seq_nums[last_sent]) + \
             packed_zero + \
             padding_sequence
         socks[last_sent].sendto(ret_msg, addr)
-        send_times[last_sent][seq_nums[last_sent]] = current_time
+        # send_times[last_sent][seq_nums[last_sent]] = current_time
         seq_nums[last_sent] += 1
         last_sent = (last_sent + 1) % 2
     print(f'{last_rtt=},{seq_nums=}')
         
-received_handshakes = 0
-while received_handshakes < 2:
-    ready_socks,_,_ = select(socks, [], []) 
-    for sock in ready_socks:
-        data, addr = sock.recvfrom(minimum_payload_size)
-        recv_seq_num = struct.unpack('!I', data[:4])[0]
-        recv_ack_num = struct.unpack('!I', data[4:8])[0]
-
-        received_handshakes += 1
+data, addr = socks[0].recvfrom(minimum_payload_size)
 
 send_and_receive_acks()
