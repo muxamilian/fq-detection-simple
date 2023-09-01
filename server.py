b26fbecc1b2bce380d810ca3acbc62a4cc8b07c1
muxamilian <maximilian.bachl@gmail.com>
2023-09-01 19:38:35 +0200

    Seems to work

--- server.py
+++ server.py
@@ -46,11 +46,15 @@ print(f'{initial_rtt=}')
 already_encountered_loss = False
 rates = [15, 30]
 latest_rtts = [initial_rtt] * len(ports)
-main_socket.setblocking(False)
+for sock in socks:
+  sock.setblocking(False)
 for cycle_num in range(sys.maxsize):
   seq_nums_beginning = list(seq_nums)
   num_acked = [0] * len(ports) 
+  first_ack_times = [None] * len(ports)
+  last_ack_times = [None] * len(ports)
   start_time = time.time()
+  send_end_time = None
   time_to_run = max(max(latest_rtts), 0.2)
   should_send = [item*time_to_run for item in rates]
   seq_nums_end = None
@@ -62,6 +66,7 @@ for cycle_num in range(sys.maxsize):
     if seq_nums_end is None and current_time > start_time + time_to_run:
       time_to_wait = 4*max(latest_rtts)
       seq_nums_end = list(seq_nums)
+      send_end_time = current_time
     elif current_time > start_time + time_to_run + time_to_wait:
       break
     try:
@@ -71,7 +76,11 @@ for cycle_num in range(sys.maxsize):
         ack_num, sock_index = struct.unpack('!IB', data[0:5])
         latest_rtts[sock_index] = current_time - send_times[sock_index][ack_num]
         if ack_num >= seq_nums_beginning[sock_index] and (seq_nums_end is None or ack_num < seq_nums_end[sock_index]):
+          if num_acked[sock_index] == 0:
+            first_ack_times[sock_index] = current_time
           num_acked[sock_index] += 1
+        elif last_ack_times[sock_index] is None and seq_nums_end is not None and ack_num >= seq_nums_end[sock_index]:
+          last_ack_times[sock_index] = current_time
         del send_times[sock_index][ack_num]
     except OSError as e:
       pass
@@ -101,21 +110,23 @@ for cycle_num in range(sys.maxsize):
     send_times[next_socket][seq_nums[next_socket]] = time.time()
     seq_nums[next_socket] += 1
   packets_actually_sent = [seq_nums_end_i-seq_nums_beginning_i for seq_nums_beginning_i, seq_nums_end_i in zip(seq_nums_beginning, seq_nums_end)]
-  sent_enough = [math.ceil(packets_actually_sent_i) + 1 >= should_send_i * 7/8 for should_send_i, packets_actually_sent_i in zip(should_send, packets_actually_sent)]
+  sent_enough = [math.ceil(packets_actually_sent_i) + 1 >= should_send_i for should_send_i, packets_actually_sent_i in zip(should_send, packets_actually_sent)]
   rtts_ms = [round(item*1000) for item in latest_rtts]
   print(f'End {cycle_num=},{packets_actually_sent=},{rtts_ms=},{sent_enough=},{seq_nums_beginning=},{should_send=},{seq_nums_end=},{num_acked=},{seq_nums=}')
   
   if not all(sent_enough):
     print('Failed to utilize the link. Aborting.')
     break
-  elif num_acked[0] < packets_actually_sent[0] * 7/8 and num_acked[1] < packets_actually_sent[1] * 7/8:
+  elif num_acked[1] < packets_actually_sent[1]:
     if not already_encountered_loss:
       already_encountered_loss = True
       acked_over_sent = num_acked[1]/packets_actually_sent[1]
       print(f'Encountered packet loss, {acked_over_sent=}')
       rates = [int(round(rate*acked_over_sent*2)) for rate in rates]
     else:
-      loss_ratio = (num_acked[0]/packets_actually_sent[0])/(num_acked[1]/packets_actually_sent[1])
+      loss_ratio = \
+        ((num_acked[0]/(last_ack_times[0]-first_ack_times[0]))/(packets_actually_sent[0]/(send_end_time-start_time)))/ \
+        ((num_acked[1]/(last_ack_times[1]-first_ack_times[1]))/(packets_actually_sent[1]/(send_end_time-start_time)))
       if loss_ratio >= 1.5:
         confidence = min((loss_ratio-1.5)*2, 1)
         print(f'Fair queuing detected with a confidence of {round(confidence*100)}%, {loss_ratio=}')
